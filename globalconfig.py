aff57eeb9546ac4637b16263ac9c0d7b52d97425
b1ueshad0w <yinguogao2005@gmail.com>
2018-03-21 21:21:47 +0800

    Update config.

--- globalconfig.py
+++ globalconfig.py
@@ -1,3 +1,4 @@
 import os
 
 CIFAR10_DATA_DIR = os.path.join(os.environ['HOME'], 'data/tensorflow/cifar10/cifar-10-batches-bin')
+MNIST_DATA_DIR = os.path.join(os.environ['HOME'], 'data/tensorflow/MNIST_data')
